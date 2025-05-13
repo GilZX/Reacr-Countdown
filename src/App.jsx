5ee195b0a6f580cb9d14b03cb34c1c87b893dfb0
GILZX <gilrz0101@gmail.com>
2025-05-13 15:28:48 -0600

    SapCountdown Comming Soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import mainLogo from './assets/logo-2.png'
 import './App.css'
 
 function App() {
@@ -9,25 +10,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+       
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={mainLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Sap CountDown</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count HH:MM:SS 
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          ....Comming soon
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
